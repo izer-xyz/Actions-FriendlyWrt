41df768e9f09557a8f68c7d1ab03d6d5bfe258bc
Péter <peter@perenyi.net>
2025-04-28 19:42:09 +1000

    merge config

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
-sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
-sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
-
 mkdir configs/rockchip-lite
 curl -o configs/rockchip-lite/01-lite https://downloads.openwrt.org/releases/24.10.0/targets/rockchip/armv8/config.buildinfo
+
+cat >> configs/rockchip-lite/01-lite <<EOL
+CONFIG_PACKAGE_ath10k-firmware-qca6174=y
+EOL
+
