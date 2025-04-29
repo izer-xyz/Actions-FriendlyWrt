b00483068336383b26d3112ae497fd26c4ab4e0e
Péter <peter@perenyi.net>
2025-04-29 20:14:57 +1000

    Add usb gadget kernel packages

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -5,5 +5,7 @@ curl -o configs/rockchip-lite/01-lite https://downloads.openwrt.org/releases/24.
 
 cat >> configs/rockchip-lite/01-lite <<EOL
 CONFIG_PACKAGE_ath10k-firmware-qca6174=y
+CONFIG_PACKAGE_kmod-usb-dwc2=y
+CONFIG_PACKAGE_kmod-usb-gadget-eth=y
 EOL
 
