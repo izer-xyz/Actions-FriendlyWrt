ecf49bec64ad5db47f4e97e87d465ca3bd7c9f74
Péter <peter@perenyi.net>
2025-04-30 10:40:42 +1000

    Add ethernet gadget kmods

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -5,7 +5,8 @@ curl -o configs/rockchip-lite/01-lite https://downloads.openwrt.org/releases/24.
 
 cat >> configs/rockchip-lite/01-lite <<EOL
 CONFIG_PACKAGE_ath10k-firmware-qca6174=y
-CONFIG_PACKAGE_kmod-usb-lib-composite=y
+CONFIG_PACKAGE_kmod-usb-gadget=y
+CONFIG_PACKAGE_kmod-usb-dwc2=y 
 CONFIG_PACKAGE_iw=y
 EOL
 
